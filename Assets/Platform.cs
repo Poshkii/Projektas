5ecc6d241ad77d49e9d1a333c55974a7951bba07
kardom1 <kardom1@ktu.lt>
2024-02-21 12:28:15 +0200

    Generavimas3

--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -6,14 +6,15 @@ using UnityEngine.UIElements;
 public class Platform : MonoBehaviour
 {
     Vector2 startPos = new Vector2 (10, -7);
-    float minGap = 0.5f;
-    float maxGap = 3;
+    float minGap = 1f;
+    float maxGap = 4;
     float yOffset = 1.5f;
     float spawnDelay = 1.5f;
     float platformSpeed = 1.5f;
     float minWidth = 0.5f;
     float maxWidth = 2f;
     bool screenFilled = false;
+    Platform spawnedPlatform = null;
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +30,7 @@ public class Platform : MonoBehaviour
         position.y = -8f;
         transform.position = position + offset;
 
-        StartCoroutine(waitToSpawn(spawnDelay));        
+        //StartCoroutine(waitToSpawn(spawnDelay));        
     }     
 
     public void SpawnRecusively(int count)
@@ -37,16 +38,30 @@ public class Platform : MonoBehaviour
         if (count > 0)
         {
             count--;
-            SpawnPlatform().SpawnRecusively(count);
+            SpawnPlatform();
+            spawnedPlatform.SpawnRecusively(count);
         }
     }
 
-    Platform SpawnPlatform()
+    void SpawnPlatform()
     {
-        GameObject spawnedPlatform = Instantiate(gameObject);
-        Platform platform = spawnedPlatform.GetComponent<Platform>();
-        platform.Spawn(transform.position);
-        return platform;
+        if (spawnedPlatform == null)
+        {
+            spawnedPlatform = Instantiate(gameObject).GetComponent<Platform>();
+            spawnedPlatform.Spawn(transform.position);
+        }        
+    }
+
+    public void SpawnOnLast()
+    {
+        if (spawnedPlatform != null)
+        {
+            spawnedPlatform.SpawnOnLast();
+        }
+        else
+        {
+            SpawnPlatform();
+        }
     }
    
     // Update is called once per frame
@@ -54,6 +69,7 @@ public class Platform : MonoBehaviour
     {       
         if (transform.position.x < -12)
         {           
+            SpawnOnLast();
             Destroy(gameObject);
         }
         transform.position = new Vector2(transform.position.x - platformSpeed * Time.deltaTime, transform.position.y);
