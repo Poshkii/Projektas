220f5b6dc533e91871e49ffe85c7be17d6920b44
Poshkii <donatasrackauskass@gmail.com>
2024-04-17 13:04:55 +0300

    logarithmic platform speed

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -142,9 +142,10 @@ public class Platform : MonoBehaviour
             model.GetComponent<ModelScript>().PlayAnimation();
         }
 
-        platformSpeed += 0.0005f * Time.deltaTime;
-
+        float logFactor = 0.01f; 
+        platformSpeed += logFactor * Mathf.Log(platformSpeed + 1) * Time.deltaTime;
         transform.position = new Vector2(transform.position.x - platformSpeed * Time.deltaTime, transform.position.y - platformSpeedVertical * Time.deltaTime);
+        Debug.Log(platformSpeed);
     }
 
     public void DropPlatform()
